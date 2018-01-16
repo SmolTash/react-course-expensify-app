0907728d6acd1d2ae35d34afeafde1aad5160a5e
SmolKa <smolka.tash@gmail.com>
2018-01-16 09:47:09 +0200

    Setup startEditExpense

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,13 @@
-import {startAddExpense, addExpense, removeExpense, editExpense, setExpenses, startSetExpenses, startRemoveExpense} from '../../actions/expenses';
+import {
+    startAddExpense, 
+    addExpense, 
+    removeExpense, 
+    editExpense, 
+    setExpenses, 
+    startSetExpenses, 
+    startRemoveExpense,
+    startEditExpense
+} from '../../actions/expenses';
 import expenses from '../fixtures/expenses';
 import configureMockStore from 'redux-mock-store';
 import thunk from 'redux-thunk';
@@ -49,6 +58,24 @@ test ('edit expense action object ', () => {
             note: 'some note'
         }
     })
+});
+
+test ('should edit expenses from firabase', (done) => {
+    const store  = createMockStore({});
+    const id = expenses[0].id;
+    const updates = {amount : 21054 };
+    store.dispatch(startEditExpense(id,updates)).then (() => {
+        const actions = store.getActions();
+        expect(actions[0]).toEqual ({
+            type: 'EDIT_EXPENSE',
+            id,
+            updates
+        });
+        return database.ref(`expenses/${id}`).once('value');       
+    }).then ((snapshot) => { 
+        expect(snapshot.val().amount).toBe(updates.amount);
+        done();
+    });
 });
 
 test ('addExpense action object  with provided value', () => {
